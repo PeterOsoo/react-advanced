a0027638283d94fab46dc0985b37b0a9598bab56
Peter Osoo <Posoo617@gmail.com>
2020-10-13 05:55:12 +0300

    useEffect - dependency array & conditional render

--- src/components/useEffect/Basics.js
+++ src/components/useEffect/Basics.js
@@ -1,12 +1,29 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 export const Basics = () => {
+	const [value, setValue] = useState(0)
+
 	useEffect(() => {
 		console.log("call useEffect")
-	}, console.log("render component"))
+
+		if (value > 0) {
+			document.title = `New Messages(${value})`
+		}
+	}, [value])
+
+	useEffect(() => {
+		console.log("hello useEffet")
+	}, [])
+
+	console.log("render component")
 	return (
-		<div>
-			<h3>Use Effect Basics</h3>
-		</div>
+		<>
+			<h1>{value}</h1>
+			<button className="btn" onClick={() => setValue(value + 1)}>
+				click me
+			</button>
+		</>
 	)
 }
+
+// empty array to run once. Only after the initial render
